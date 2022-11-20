f7592b8d458e6f71e5de9a23f3b3ed895ecc1c19
seigo07 <seven_8s@icloud.com>
2022-11-20 03:27:23 +0000

    fix binaries

--- src/binaries/BinaryConstraint.java
+++ src/binaries/BinaryConstraint.java
@@ -1,22 +1,33 @@
-import java.util.* ;
+import java.util.*;
 
 public final class BinaryConstraint {
-  private int firstVar, secondVar ;
-  private ArrayList<BinaryTuple> tuples ;
-  
+  private int firstVar, secondVar;
+  private ArrayList<BinaryTuple> tuples;
+
   public BinaryConstraint(int fv, int sv, ArrayList<BinaryTuple> t) {
-    firstVar = fv ;
-    secondVar = sv ;
-    tuples = t ;
+    firstVar = fv;
+    secondVar = sv;
+    tuples = t;
   }
-  
+
   public String toString() {
-    StringBuffer result = new StringBuffer() ;
-    result.append("c("+firstVar+", "+secondVar+")\n") ;
+    StringBuffer result = new StringBuffer();
+    result.append("c(" + firstVar + ", " + secondVar + ")\n");
     for (BinaryTuple bt : tuples)
-      result.append(bt+"\n") ;
-    return result.toString() ;
+      result.append(bt + "\n");
+    return result.toString();
   }
-  
-  // SUGGESTION: You will want to add methods here to reason about the constraint
+
+  // NEWS
+  public int getFirstVar() {
+    return firstVar;
+  }
+
+  public int getSecondVar() {
+    return secondVar;
+  }
+
+  public ArrayList<BinaryTuple> getTuples() {
+    return tuples;
+  }
 }
